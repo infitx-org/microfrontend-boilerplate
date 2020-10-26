16c4dc7098ec5926d8b6693671cfd1d4e3439126
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2020-10-26 23:08:17 +0100

    Use latest version of ui-components

--- src/Injector.tsx
+++ src/Injector.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Provider } from 'react-redux';
 import { Store, combineReducers } from 'redux';
 import { useReducerLoader } from '@modusbox/modusbox-ui-components/dist/redux/hooks';
+import { InjectableStore } from '@modusbox/modusbox-ui-components/dist/redux/injectors';
 import { App } from './App';
 import { reducers } from './App/reducer';
 import sagas from './App/sagas';
@@ -11,14 +12,14 @@ interface AppWithStoreInjectorProps {
 }
 
 const AppWithStoreInjector: FC<AppWithStoreInjectorProps> = ({ token }) => {
-  const store: Store | false = useReducerLoader(combineReducers(reducers), sagas);
+  const store = useReducerLoader(combineReducers(reducers), sagas);
 
   if (store === false) {
     return <div>error while loading reducer / sagas</div>;
   }
 
   return (
-    <Provider store={store as Store}>
+    <Provider store={store}>
       <App token={token} />
     </Provider>
   );
